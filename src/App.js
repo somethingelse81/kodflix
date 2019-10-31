7c584c86c98939d58206970fdaee6f2a64809ff2
Sanjay <sb@Stuarts-Air.localdomain>
2019-10-31 16:23:52 +0000

    added movie cover added 6 movie show titles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import avengersendgame from "./avengersendgame.jpeg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <p>Kodflix</p>
+      <header className="moviecoverheader">
+        <img src={avengersendgame} alt="avengers end game moie cover" />
+      </header>
+      <section className="moviecoverrow">
+        <figure classname="moviecover">Black Mirror</figure>
+        <figure classname="moviecover">breakingbad</figure>
+        <figure classname="moviecover">deathnote</figure>
+      </section>
+      <section className="moviecoverrow">
+        <figure classname="moviecover">Game Of Thrones</figure>
+        <figure classname="moviecover">The Walking Dead</figure>
+        <figure classname="moviecover">The Wire</figure>
+      </section>
     </div>
   );
 }

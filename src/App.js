d97179119fc336c17101b14fe17f73fd5f9ce23c
Sanjay <sb@Sanjs-MacBook-Pro.local>
2019-11-05 15:19:53 +0000

    Replaced TV show titles with covers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
-import avengersendgame from "./avengersendgame.jpeg";
+import avengersendgame from "./images/avengersendgame.jpeg";
+import  blackmirror from "./images/blackmirror.jpg";
+import  breakingbad from "./images/breakingbad.jpg";
+import  deathnote from "./images/deathnote.jpg";
+import  got from "./images/got.jpg";
+import  thewalkingdead from "./images/thewalkingdead.jpg";
+import  thewire from "./images/thewire.jpg";
+
 import "./App.css";
 
 function App() {
@@ -9,14 +16,14 @@ function App() {
         <img src={avengersendgame} alt="avengers end game moie cover" />
       </header>
       <section className="moviecoverrow">
-        <figure classname="moviecover">Black Mirror</figure>
-        <figure classname="moviecover">Breakingbad</figure>
-        <figure classname="moviecover">Deathnote</figure>
+        <figure classname="moviecover"><img src={blackmirror} alt='blackmirror'/></figure>
+        <figure classname="moviecover"><img src={breakingbad} alt='breakingbad' /></figure>
+        <figure classname="moviecover"><img src={deathnote} alt='deathnote' /></figure>
       </section>
       <section className="moviecoverrow">
-        <figure classname="moviecover">Game Of Thrones</figure>
-        <figure classname="moviecover">The Walking Dead</figure>
-        <figure classname="moviecover">The Wire</figure>
+        <figure classname="moviecover"><img src={got} alt='got' /></figure>
+        <figure classname="moviecover"><img src={thewalkingdead} alt='thewalkingdead' /></figure>
+        <figure classname="moviecover"><img src={thewire} alt='thewire' /></figure>
       </section>
     </div>
   );

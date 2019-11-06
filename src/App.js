783b79fa58d69931a941f26f44dd9535ca481994
Sanjay <sb@Sanjs-MacBook-Pro.local>
2019-11-06 15:55:22 +0000

    Added the TV show title overlay on hover-over

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import avengersendgame from "./images/avengersendgame.jpeg";
-import  blackmirror from "./images/blackmirror.jpg";
-import  breakingbad from "./images/breakingbad.jpg";
-import  deathnote from "./images/deathnote.jpg";
-import  got from "./images/got.jpg";
-import  thewalkingdead from "./images/thewalkingdead.jpg";
-import  thewire from "./images/thewire.jpg";
+import blackmirror from "./images/blackmirror.jpg";
+import breakingbad from "./images/breakingbad.jpg";
+import deathnote from "./images/deathnote.jpg";
+import got from "./images/got.jpg";
+import thewalkingdead from "./images/thewalkingdead.jpg";
+import thewire from "./images/thewire.jpg";
 
 import "./App.css";
 
@@ -16,14 +16,44 @@ function App() {
         <img src={avengersendgame} alt="avengers end game moie cover" />
       </header>
       <section className="moviecoverrow">
-        <figure classname="moviecover"><img src={blackmirror} alt='blackmirror'/></figure>
-        <figure classname="moviecover"><img src={breakingbad} alt='breakingbad' /></figure>
-        <figure classname="moviecover"><img src={deathnote} alt='deathnote' /></figure>
+        <figure className="moviecover">
+          <img src={blackmirror} alt="blackmirror" />
+          <div className="overlay">
+            <h1>Black Mirror</h1>
+          </div>
+        </figure>
+        <figure className="moviecover">
+          <img src={breakingbad} alt="breakingbad" />
+          <div className="overlay">
+            <h1>Breaking Bad</h1>
+          </div>
+        </figure>
+        <figure className="moviecover">
+          <img src={deathnote} alt="deathnote" />
+          <div className="overlay">
+            <h1>Death Note</h1>
+          </div>
+        </figure>
       </section>
       <section className="moviecoverrow">
-        <figure classname="moviecover"><img src={got} alt='got' /></figure>
-        <figure classname="moviecover"><img src={thewalkingdead} alt='thewalkingdead' /></figure>
-        <figure classname="moviecover"><img src={thewire} alt='thewire' /></figure>
+        <figure className="moviecover">
+          <img src={got} alt="got" />
+          <div className="overlay">
+            <h1>Game Of Thrones</h1>
+          </div>
+        </figure>
+        <figure className="moviecover">
+          <img src={thewalkingdead} alt="thewalkingdead" />
+          <div className="overlay">
+            <h1>The Walking Dead</h1>
+          </div>
+        </figure>
+        <figure className="moviecover">
+          <img src={thewire} alt="thewire" />
+          <div className="overlay">
+            <h1>The Wire</h1>
+          </div>
+        </figure>
       </section>
     </div>
   );

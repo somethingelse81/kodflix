585d85891f371e5fabd4c803f0af6ce132754234
Sanjay <sanjay.bagtharia@gmail.com>
2019-11-11 17:56:20 +0000

    Introduced routing / navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Details from "./Details";
 import MovieHeader from "./MovieHeader";
 import Gallery from "./Gallery";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <MovieHeader />
-      <Gallery />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path='/' component={MovieHeader} />
+        <Route exact path='/' component={Gallery} />
+        <Route exact path='/details' component={Details} />
+
+      </div>
+    </Router>
   );
 }
 export default App;
---
+++ src/Details.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+export default function Details() {
+  return (
+    <>
+        <div>details</div>
+      <Link to='/'>Back to homepage</Link>
+    </>
+  );
+}

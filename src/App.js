7f54ee71fbb2d3eec2e3dcaf1a55ec36d5dbb633
Sanjay <sanjay.bagtharia@gmail.com>
2019-11-12 14:03:51 +0000

    code clean up

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
         <Route exact path='/' component={MovieHeader} />
         <Route exact path='/' component={Gallery} />
         <Route exact path='/details' component={Details} />
-
       </div>
     </Router>
   );
--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Movie(props) {
   return (
-    <Link to='details'>
-    <figure className="moviecover">
+    <Link to="details" className="moviecover">
       <img src={props.img} alt={`${props.alt} Movie`} />
       <div className="overlay">
         <h1>{props.title}</h1>
       </div>
-    </figure>
     </Link>
   );
 }
